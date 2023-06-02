59547e4ec3db9517fb8c9f578035bc5189ea05f6
cq2n-iwym <naoki.iwayama@asahi-net.com>
2023-06-02 17:15:52 +0900

    ok

--- flake.nix
+++ flake.nix
@@ -3,109 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nix-alien.url = "github:thiagokokada/nix-alien";
-    nix-alien.inputs.nixpkgs.follows = "nixpkgs";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    neovim-nightly-overlay.inputs.flake-compat.follows = "flake-compat";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
-    # mine
-    my-fzf-wrapper.url = "github:Hogeyama/my-fzf-wrapper";
   };
 
   outputs =
     { self
     , nixpkgs
-    , nixpkgs-unstable
-    , sops-nix
     , home-manager
-    , neovim-nightly-overlay
     , nix-index-database
-    , nix-alien
-    , my-fzf-wrapper
     , ...
     }:
     let
-      system = "x86_64-linux";
-
       env = import ./env.nix;
-      hostName = env.hostName;
-      username = env.user.name;
-
-      my-overlay = final: prev: {
-        # unstable packages are available as pkgs.unstable.${package}
-        unstable = builtins.getAttr system nixpkgs-unstable.outputs.legacyPackages;
-        # my packages
-        illusion = import ./pkgs/illusion { pkgs = final; };
-        Cica = import ./pkgs/Cica { inherit (final) fetchzip unzip; };
-        amazon-corretto17 = import ./pkgs/amazon-corretto17 { pkgs = final; };
-        aws2-wrap = import ./pkgs/aws2-wrap { pkgs = final; };
-
-        my-xmobar = import ./pkgs/my-xmobar { pkgs = final; };
-        my-xmonad = import ./pkgs/my-xmonad { pkgs = final; };
-        my-fzf = import ./pkgs/my-fzf { pkgs = final; };
-        my-fzf-wrapper = my-fzf-wrapper.outputs.packages.${system}.default;
-      };
-
-      overlays = [
-        my-overlay
-        neovim-nightly-overlay.overlay
-        nix-alien.overlays.default
-      ];
-
-      pkgs = import nixpkgs { inherit system overlays; };
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
     in
     {
-      # For NixOS
-      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          # overlay
-          (_: { nixpkgs.overlays = overlays; })
-          # system configuration
-          ({ config, pkgs, ... }: import ./configuration.nix ({
-            inherit config pkgs nixpkgs;
-          }))
-          # home-manager configuration
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} =
-              { config, pkgs, ... }: import ./home.nix { inherit config pkgs self; };
-            home-manager.sharedModules = [
-              nix-index-database.hmModules.nix-index
-            ];
-          }
-          # sops-nix
-          sops-nix.nixosModules.sops
-          # env-unique config
-          env.extraConfig
-        ];
-      };
-
       # For Nix package manager only
-      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.${env.username} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
-          ({ config, pkgs, ... }: import ./home.nix { inherit config pkgs self; })
-          {
-            home = {
-              inherit username;
-              homeDirectory = "/home/${username}";
-              stateVersion = "22.11";
-            };
-          }
+          ({ config, pkgs, ... }: import ./home.nix { inherit config pkgs self env; })
+          ({ config, pkgs, ... }: env.extraConfig { inherit config pkgs self; })
+          nix-index-database.hmModules.nix-index
         ];
       };
-
-      devShells.${system}.xmonad = import ./pkgs/my-xmonad/shell.nix { inherit pkgs; };
     };
 }
--- home.nix
+++ home.nix
@@ -1,56 +1,11 @@
 # https://rycee.gitlab.io/home-manager/options.html
-{ config, self, pkgs, ... }:
-let
-  env = import ./env.nix;
-
-  dotfilesSymlinks =
-    { rootDir ? "files"
-    , clonedPath ? "${config.home.homeDirectory}/.config/home-manager"
-    }:
-    let
-      # /nix/store/.../files 以下を traverse して相対パスを取得する。
-      # それを "${config.home.homeDirectory}/nix-config/${rootDir}" 以下の絶対パスに変換して、
-      # $HOME 以下にシンボリックリンクを貼る。
-      linksRootDirInStore = "${self}/${rootDir}";
-      linksRootDirInVCS = "${clonedPath}/${rootDir}";
-      toAbsPathInStore = s: "${linksRootDirInStore}/${s}";
-      toAbsPathInVCS = s: "${linksRootDirInVCS}/${s}";
-      dotfilesSymlinks' = dirPath:
-        let
-          # 相対パスの取得
-          items = builtins.readDir (toAbsPathInStore dirPath);
-          funcInner = name: type:
-            let
-              newDirPath = if dirPath == "" then name else "${dirPath}/${name}";
-              fileOrSymlink =
-                {
-                  "${newDirPath}" = {
-                    source = config.lib.file.mkOutOfStoreSymlink
-                      (toAbsPathInVCS newDirPath);
-                  };
-                };
-              cases = {
-                "regular" = fileOrSymlink;
-                "symlink" = fileOrSymlink;
-                "directory" = dotfilesSymlinks' newDirPath;
-              };
-              otherwise = abort ("unknown item type: " + toAbsPathInVCS newDirPath);
-            in
-              cases.${type} or otherwise;
-        in
-        pkgs.lib.concatMapAttrs (name: type: funcInner name type) items;
-    in
-    dotfilesSymlinks' "";
-in
+{ config, pkgs, ... }:
 {
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
   home = {
     stateVersion = "22.11";
+    homeDirectory = "/home/${config.home.username}";
     packages = with pkgs; [
       albert
-      aws2-wrap
       awscli2
       aws-sam-cli
       aws-vault
@@ -90,6 +45,8 @@ in
       manix
       mercurial
       neovim-remote
+      nil
+      nixpkgs-fmt
       nix-du
       nix-tree
       nodejs
@@ -123,365 +80,35 @@ in
       wget
       yq
       zip
-      ### my packages
-      my-fzf-wrapper
     ];
-    file = dotfilesSymlinks { } // {
-    };
-    sessionVariables = {
-      EDITOR = "nvimw";
-      BROWSER = env.user.browser;
-
-      # aws
-      AWS_PAGER = "";
-      AWS_DEFAULT_OUTPUT = "yaml";
-      AWS_CLI_AUTO_PROMPT = "on-partial";
-
-      # aws-vault
-      AWS_VAULT_BACKEND = "pass";
-      AWS_VAULT_PASS_PREFIX = "aws-vault/";
-
-      # direnv
-      DIRENV_LOG_FORMAT = "";
-
-      # my-fzf-wrapper
-      FZFW_FD_EXCLUDE_PATHS = ".git,.hg,.hie,dist-newstyle,__pycache__,Session.vim";
-
-      # nix-ld
-      # 典型的な実行ファイルはそのまま動くようにしておく。
-      # NIX_LD_LIBRARY_PATH は必要そうなものを適宜足していく運用にする。
-      # pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; の方が正しそうだが
-      # access to canonical path is forbidden in restricted mode エラーが出るので妥協。
-      NIX_LD = "${pkgs.glibc}/lib/ld-linux-x86-64.so.2";
-      NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
-        stdenv.cc.cc
-        zlib
-        ncurses
-        gmp5
-      ];
-    };
   };
   programs = {
-    home-manager = {
-      enable = true;
-    };
-    direnv = {
-      enable = true;
-      nix-direnv.enable = false;
-    };
-    atuin = {
-      enable = true;
-      flags = [ "--disable-up-arrow" ];
-    };
-    fzf = {
-      enable = false;
-      enableZshIntegration = false;
-    };
-    neovim = {
-      enable = true;
-      package = pkgs.neovim-nightly;
-      withNodeJs = true;
-      withPython3 = true;
-      extraConfig = ''
-        source  ~/.config/nvim/real-init.vim
-      '';
-    };
-    tmux = {
-      enable = true;
-      terminal = "tmux-256color";
-      plugins = with pkgs; [
-        {
-          plugin = tmuxPlugins.logging;
-          extraConfig = ''
-            # log
-            set-option -g @logging_key "M-p"
-            set-option -g @logging-path "$HOME/log/tmux"
-            set-option -g @logging-filename "%Y%m%dT%H%M%S.log"
-            # history
-            set-option -g @save-complete-history-key "P"
-            set-option -g @save-complete-history-path "$HOME/log/tmux"
-            set-option -g @save-complete-history-filename "%Y%m%dT%H%M%S.history"
-            set-option -g history-limit 100000
-            # screen-capture
-            set-option -g @screen-capture-key "M-Z"
-          '';
-        }
-        {
-          plugin = tmuxPlugins.jump;
-          extraConfig = ''
-            set -g @jump-key 's'
-          '';
-        }
-        {
-          plugin = tmuxPlugins.power-theme;
-          extraConfig = ''
-            set -g @tmux_power_theme 'snow'
-          '';
-        }
-      ];
-      extraConfig = ''
-        ################################################################################
-        # Basic
-        ################################################################################
-
-        # prefix=C-q
-        set -g prefix C-q
-        # Enable Italic
-        set-option -g default-terminal "tmux-256color"
-        # Enable True Color on xterm-256color
-        set-option -ga terminal-overrides ",xterm-256color:Tc"
-        # status bar on top
-        set-option -g status-position top
-        # no mouse
-        set-option -g mouse off
-
-        ################################################################################
-        # Pane
-        ################################################################################
-
-        # vimのキーバインドでペインを移動する
-        bind h select-pane -L
-        bind j select-pane -D
-        bind k select-pane -U
-        bind l select-pane -R
-
-        # vimのキーバインドでペインをリサイズする
-        bind -r H resize-pane -L 5
-        bind -r J resize-pane -D 5
-        bind -r K resize-pane -U 5
-        bind -r L resize-pane -R 5
-
-        # | でペインを縦分割する
-        bind | split-window -h
-
-        # - でペインを縦分割する
-        bind - split-window -v
-
-        ################################################################################
-        # Copy / Paste
-        ################################################################################
-
-        setw -g mode-keys vi
-
-        bind C-q copy-mode
-
-        bind -T copy-mode-vi v send -X begin-selection
-        bind -T copy-mode-vi V send -X select-line
-        bind -T copy-mode-vi C-v send -X rectangle-toggle
-
-        bind -T copy-mode-vi K send-keys -X -N 5 scroll-up
-        bind -T copy-mode-vi J send-keys -X -N 5 scroll-down
-
-        bind -T copy-mode-vi Enter send-keys -X copy-pipe-and-cancel "myclip"
-        bind -T copy-mode-vi y     send-keys -X copy-pipe-and-cancel "myclip"
-      '';
-    };
-    zsh = {
-      enable = true;
-      enableAutosuggestions = true;
-      enableSyntaxHighlighting = true;
-      enableVteIntegration = true;
-      autocd = true;
-      history.extended = true;
-      oh-my-zsh = {
-        enable = true;
-        theme = "frisk";
-        plugins = [
-          # [tool]
-          "aws"
-          "docker"
-          "docker-compose"
-          "fd"
-          "gcloud"
-          "git"
-          "git-auto-fetch"
-          "gh"
-          "ripgrep"
-          "rsync"
-          "ssh-agent"
-          "zoxide"
-          # [language]
-          "cabal"
-          "stack"
-          "rust"
-          "node"
-          "npm"
-          "deno"
-        ];
-        extraConfig = ''
-          # do not load any identities on start
-          zstyle :omz:plugins:ssh-agent lazy yes
-          # home-manager's module seems broken. Manually set fpath
-          fpath+=(${pkgs.watson}/share/zsh/site-functions)
-          fpath+=(${pkgs.just}/share/zsh/site-functions)
-          fpath+=(${pkgs.pass}/share/zsh/site-functions)
-          # dasel completion
-          fpath+=(${pkgs.stdenv.mkDerivation {
-            name = "dasel-completion";
-            unpackPhase = "true";
-            buildInputs = [ pkgs.unstable.dasel ];
-            installPhase = ''
-              mkdir -p $out/share/zsh/site-functions
-              dasel --version
-              dasel completion zsh > $out/share/zsh/site-functions/_dasel
-            '';
-          }}/share/zsh/site-functions)
-          # for ddc-zsh
-          zmodload zsh/zpty
-        '';
-      };
-      envExtra = ''
-        source-if-exists() {
-          [ -e $1 ] && . $1
-        }
-        test -z "''${ZSHENV_LOADED}" || return
-        export ZSHENV_LOADED=1
-        export PATH="$HOME/.local/bin:$PATH" # prefer .local/bin
-        source-if-exists "$HOME/.ghcup/env"
-        source-if-exists "$HOME/.cargo/env"
-        source-if-exists "$HOME/.opam/opam-init/init.zsh"
-        source-if-exists "$HOME/.nix-profile/etc/profile.d/nix.sh"
-        source-if-exists "$HOME/.autojump/etc/profile.d/autojump.sh"
-        source-if-exists "$HOME/.poetry/env"
-        source-if-exists "$HOME/.zshenv.local"
-        export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude .git'
-        export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-        export JAVA8_HOME=${pkgs.openjdk8}/lib/openjdk
-        export JAVA11_HOME=${pkgs.openjdk11}/lib/openjdk
-        export JAVA17_HOME=${pkgs.amazon-corretto17}
-        export JAVA_HOME="''$JAVA17_HOME"
-        export PATH="''$PATH:''$JAVA_HOME/bin"
-      '';
-      initExtra = ''
-        zstyle ':completion:*' verbose yes
-        zstyle ':completion:*' format '%B%d%b'
-        zstyle ':completion:*:warnings' format 'No matches for: %d'
-        # https://stackoverflow.com/questions/24226685
-        zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-        # drwxrwxrwxなるディレクトリ(other writable)が見にくいのを直す
-        eval $(dircolors | sed 's/ow=[^:]*:/ow=01;34:/') #青背景白文字
-        # bindkey
-        bindkey "^K" up-line-or-history
-        bindkey "^J" down-line-or-history
-        bindkey "^I" expand-or-complete-prefix
-        # functions
-        cd-ls(){
-          \cd $* && exa -s name
-        }
-        mkcd(){
-          mkdir -p "$1" && cd "$1"
-        }
-        ncd() {
-          nvr -c "cd $(realpath $1)"
-        }
-        # なぜか fzf の completion がうまく動かないのでもう一度読む
-        export FZF_COMPLETION_TRIGGER="::"
-        . ${pkgs.fzf}/share/fzf/completion.zsh
-        # source machine local configuration
-        source-if-exists $HOME/.zshrc.local
-      '';
-      shellAliases = {
-        ls = "exa -s name";
-        cd = "cd-ls";
-        mv = "mv -i";
-        cp = "cp -iL";
-        l = "ls -F";
-        ll = "ls -ahlF";
-        la = "ls -a";
-        DU = "du -hd1 | sort -h";
-        open = "xdg-open";
-        v = "nvimw";
-        gs = "git status";
-        glog = "git log --pretty=format:\"%C(yellow)%h%Creset %C(green)%ad%Creset %s %Cred%d%Creset %Cblue[%an]\" --date=short --graph";
-        j = "just";
-        da = "direnv allow";
-        dr = "direnv reload";
-        nb = "nix build";
-        awslocal = ''AWS_ACCESS_KEY_ID=dummy AWS_SECRET_ACCESS_KEY=dummy AWS_DEFAULT_REGION=''${DEFAULT_REGION:-''${AWS_DEFAULT_REGION:-ap-northeast-1}} aws --endpoint-url=http://''${LOCALSTACK_HOST:-localhost.localstack.cloud}:4566'';
-      };
-    };
+    home-manager.enable = true;
+    direnv.enable = true;
     git = {
       enable = true;
-      inherit (env.user.git) userName userEmail;
+      # Linux環境でデフォルトにしてよさそうな設定を書いている
       extraConfig = {
-        alias.stash-all = "stash save --include-untracked";
+        init.defaultBranch = "main";
         core.autoCRLF = false;
         core.autoLF = false;
         core.quotePath = false;
-        diff.external = "difft";
         fetch.prune = true;
-        init.defaultBranch = "main";
         blame.date = "short";
         merge.ff = false;
         merge.conflictstyle = "diff3";
-        merge.tool = "my-nvimdiff3";
-        mergetool.my-nvimdiff3.cmd = "nvim -d -c 'wincmd J' $MERGED $LOCAL $BASE $REMOTE";
         pull.rebase = true;
         push.autoSetupRemote = true;
         push.default = "current";
         rebase.autoStash = true;
-        rebase.updateRefs = true;
         rebase.missingCommitsCheck = "warn";
-        rebase.abbreviateCommands = true;
         rerere.enabled = true;
         credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+        # 環境ごとの設定は ~/.config/git/config.local から読む
         include.path = "config.local";
       };
-      delta.enable = true;
     };
-    zoxide = {
-      enable = true;
-    };
-    gnome-terminal = {
-      enable = false;
-      showMenubar = false;
-      profile = {
-        test = {
-          default = true;
-          visibleName = "test";
-          font = "Illusion N 13";
-          allowBold = true;
-          audibleBell = false;
-          colors = {
-            foregroundColor = "#cacacececdcd";
-            backgroundColor = "#111112121313";
-            boldColor = "#cacacececdcd";
-            cursor = {
-              foreground = "#111112121313";
-              background = "#cacacececdcd";
-            };
-            palette = [
-              "#323232323232"
-              "#c2c228283232"
-              "#8e8ec4c43d3d"
-              "#e0e0c6c64f4f"
-              "#4343a5a5d5d5"
-              "#8b8b5757b5b5"
-              "#8e8ec4c43d3d"
-              "#eeeeeeeeeeee"
-              "#323232323232"
-              "#c2c228283232"
-              "#8e8ec4c43d3d"
-              "#e0e0c6c64f4f"
-              "#4343a5a5d5d5"
-              "#8b8b5757b5b5"
-              "#8e8ec4c43d3d"
-              "#ffffffffffff"
-            ];
-
-          };
-        };
-      };
-    };
-    navi = {
-      enable = true;
-    };
-    starship = {
-      enable = true;
-    };
-    nix-index = {
-      enable = true;
-    };
+    nix-index.enable = true;
   };
   manual.manpages.enable = false;
 }
